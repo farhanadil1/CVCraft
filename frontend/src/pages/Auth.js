29b0c6f424c2b51aaf16a21e49e059b114c66e3d
Md Adil Farhan <imfarhan574@gmail.com>
2025-10-28 11:44:54 +0530

    Render api integration

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -64,7 +64,7 @@ export default function Auth() {
     }
 
     setLoading(true);
-    const API_BASE = "http://localhost:8000/api";
+    const API_BASE = "https://cvcraft-vxk1.onrender.com/api";
 
     try {
       if (isLogin) {
